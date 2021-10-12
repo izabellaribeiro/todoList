f4ce2424d14955a75bd247f300f0a1f6dfe0b1d8
izabella.silva <izabella.silva@ccc.ufcg.edu.br>
2021-10-12 15:02:08 -0300

    bug fix in todo's :eyes:

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,16 @@ const App = () => {
     setList(newList);
   }
 
+  const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list];
+    for(let i in newList) {
+      if(newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  }
+
   return (
     <C.Container>
       <C.Area>
@@ -34,7 +44,11 @@ const App = () => {
         <AddArea onEnter = {handleAddTask} />
         
         {list.map((item, index) => (
-          <ListItem key = {index} item = {item} ></ListItem>
+          <ListItem 
+          key = {index} 
+          item = {item} 
+          onChange = {handleTaskChange}
+          />
         ))}
         
       </C.Area>
--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,22 +1,20 @@
-import {useState} from 'react';
 import * as C from "./styles";
 import { Item } from '../../types/item';
 
 type Props = {
     item: Item
+    onChange:(id:number, done: boolean) => void
 } 
 
-export const ListItem = ({item} : Props) => {
-    const [isChecked, setIsChecked] = useState (item.done);
-
+export const ListItem = ({item, onChange} : Props) => {
     return (
-        <C.Container done = {isChecked}>
-            <input 
-                type="checkbox" 
-                checked = {isChecked}
-                onChange = {e => setIsChecked(e.target.checked)}
+        <C.Container done={item.done}>
+            <input
+                type="checkbox"
+                checked={item.done}
+                onChange={e => onChange(item.id, e.target.checked)}
             />
             <label>{item.name}</label>
         </C.Container>
-    )
+    );
 }
